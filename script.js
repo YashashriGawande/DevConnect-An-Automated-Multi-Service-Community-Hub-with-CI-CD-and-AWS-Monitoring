cd2e37611f8cd230366e2aef5bdeb6782dace4d6
Yashashri Gawande <yashashrigawande1@gmail.com>
2025-05-29 11:34:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DevConnect loaded successfully!');
+  // Future interactive functionality can go here
+});
