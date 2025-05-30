eefc6c7971cf9675bf5a65e0b96e6a51c4747588
Yashashri Gawande <yashashrigawande1@gmail.com>
2025-05-30 14:19:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('DevConnect loaded successfully!');
-  // Future interactive functionality can go here
-});
