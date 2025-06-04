5763550e94f112df10183e4592c2cab227cc7960
Yashashri Gawande <yashashrigawande1@gmail.com>
2025-06-04 21:43:14 +0530

    Update access.sh

--- access.sh
+++ access.sh
@@ -2,7 +2,7 @@
 # This script is used to get the argocd, prometheus & grafana urls & credentials
 
 aws configure
-aws eks update-kubeconfig --region "us-east-1" --name "devconnect-cluster"
+aws eks update-kubeconfig --region "us-east-1" --name "dev-cluster"
 
 # ArgoCD Access
 argo_url=$(kubectl get svc -n argocd | grep argocd-server | awk '{print$4}' | head -n 1)
